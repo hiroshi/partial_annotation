f07fb800c125c810589606652f52b19b36bc40e0
Hiroshi Saito <hiroshi3110@gmail.com>
2009-06-16 17:58:40 +0900

    Fixed a issue #1, error with FormBuilder.

--- lib/partial_annotation.rb
+++ lib/partial_annotation.rb
@@ -2,8 +2,13 @@
 ActionView::Partials.module_eval do
   def render_partial_with_annotation(options={})
     original = render_partial_without_annotation(options)
-    path = _pick_partial_template(options[:partial]).template_path
-    "\n<!-- begin partial \"#{path}\" -->\n#{original}\n<!-- end partial \"#{path}\" -->\n"
+    case options[:partial]
+    when String, Symbol, NilClass
+      path = _pick_partial_template(options[:partial]).template_path
+      "\n<!-- begin partial \"#{path}\" -->\n#{original}\n<!-- end partial \"#{path}\" -->\n"
+    else
+      original
+    end
   end
   alias_method_chain :render_partial, :annotation
 end
--- test/partial_annotation_test.rb
+++ test/partial_annotation_test.rb
@@ -1,8 +1,11 @@
 require 'test_helper'
 
 class PartialAnnotationTest < ActiveSupport::TestCase
+  def setup
+    @view = ActionView::Base.new("#{File.dirname(__FILE__)}/fixtures/template")#, {}, ActionController::Base.new)
+  end
+
   test "basic" do
-    @view = ActionView::Base.new("#{File.dirname(__FILE__)}/fixtures/template")
     result = @view.render(:inline => <<-INLINE)
 before partial
   <%= render :partial => '/foo' %>
@@ -16,6 +19,27 @@ before partial
 foo
 
 <!-- end partial "_foo.html.erb" -->
+
+after partial
+    RESULT
+  end
+
+  test "with form_builder" do
+    form = ActionView::Helpers::FormBuilder.new(:foo, @foo, @view, {}, Proc.new{})
+    @view.instance_variable_set("@form", form)
+    result = @view.render(:inline => <<-INLINE)
+before partial
+  <%= render :partial => @form %>
+after partial
+    INLINE
+
+    assert_equal result, <<-RESULT
+before partial
+  
+<!-- begin partial "_form.html.erb" -->
+form
+
+<!-- end partial "_form.html.erb" -->
 
 after partial
     RESULT
